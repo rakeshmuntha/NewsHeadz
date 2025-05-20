a04e54419f7ca156228c1be274aa78a2f574fef2
rakeshmuntha <163980713+rakeshmuntha@users.noreply.github.com>
2025-05-20 13:59:11 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-black" style={{ height: "70px", textAlign: "center", justifyContent: "center", alignItems: "center" }}>
                 <div className="container-fluid" style={{ paddingLeft: "0px", paddingRight: "0px" }}>
-                    <Link className="navbar-brand" to="/"style={{paddingLeft : "20px"}}>News Donkey</Link>
+                    <Link className="navbar-brand" to="/"style={{paddingLeft : "20px"}}>NewsHeadz</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
